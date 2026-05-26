8c292abe8d8c9cb8e57e56bbcca64388f176e880
Fastops07 <matthias.voiturier@hotmail.com>
2026-05-26 10:56:39 +0200

    strcpy

---
+++ exam_rank_02/strcpy/strcpy.c
@@ -0,0 +1,34 @@
+int ft_strlen(char *s)
+{
+	int i;
+	i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char    *ft_strcpy(char *s1, char *s2)
+{
+	// A VOIR SI SIZE T OK mais osef
+	int s2_len = ft_strlen(s2);
+	int i = 0;
+
+	while(s1 && i < s2_len)
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return (s1);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	char *s1 = malloc(6);
+	char *s2 = "Salut";
+	char *res = ft_strcpy(s1,s2);
+	printf("S1 : %s\nRes :%s\n", s1,res);
+}
