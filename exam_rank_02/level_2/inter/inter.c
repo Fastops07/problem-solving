d896d6994196ea8b93844821e48f5fe7f3f76748
Fastops07 <matthias.voiturier@hotmail.com>
2026-06-17 18:07:06 +0200

    inter

---
+++ exam_rank_02/level_2/inter/inter.c
@@ -0,0 +1,54 @@
+#include <unistd.h>
+void put_char(char c)
+{
+    write (1,&c,1);
+}
+int contains(char c, char *accept)
+{
+    int i = 0;
+    while (accept[i])
+    {
+        if(c == accept[i])
+            return 1;
+        i++;
+    }
+    return 0;
+}
+
+int n_contains(char c, char *str, int n)
+{
+    int i = 0;
+    while (str[i] && i < n)
+    {
+        if(str[i] == c)
+            return 1;
+        i++;
+    }
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    if(argc != 3)
+    {
+        put_char('\n');
+        return (0);
+       
+    }
+    char *str = argv[1];
+    char *accept = argv[2];
+    if(!str || !accept)
+    {
+        put_char('\n');
+        return (0);
+    }
+
+    int i = 0;
+    while (str[i])
+    {
+        if(contains(str[i],accept) && !(n_contains(str[i], str,i)))
+            put_char(str[i]);
+        i++;
+    }
+    put_char('\n');
+}
