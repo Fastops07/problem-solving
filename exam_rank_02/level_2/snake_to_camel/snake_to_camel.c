4032b2b2efc24ba15716015b6a86db792b6068db
Fastops07 <matthias.voiturier@hotmail.com>
2026-06-18 11:49:56 +0200

    snake_to_camel

---
+++ exam_rank_02/level_2/snake_to_camel/snake_to_camel.c
@@ -0,0 +1,65 @@
+#include <unistd.h>
+void ft_put_char(char c)
+{
+    write(1,&c,1);
+}
+
+void put_nl(void)
+{
+    ft_put_char('\n');
+}
+
+int is_underscore(char c)
+{
+    return (c == '_');
+}
+int is_min(char c)
+{
+    return (c >= 'a' && c <= 'z');
+}
+int print_to_new_format(char *str)
+{
+    if(str[0] && str[1] && 
+        str[0] == '_' && is_min(str[1]))
+    {
+        ft_put_char (str[1] - 32);
+        return 2;
+    }
+    return -1;
+    
+}
+
+int main(int argc, char ** argv)
+{
+    if(argc != 2)
+    {
+        put_nl();
+        return 0;
+    }
+   
+    char *str = argv[1];
+    int i = 0;
+    int print_return;
+    while(str[i])
+    {
+        while(str[i] && !(is_underscore(str[i])))
+        {
+            ft_put_char(str[i]);
+            i++;
+        }
+        if(!str[i])
+        {
+            put_nl();
+            return 0;
+        }
+        print_return = print_to_new_format(str + i);
+        if(print_return == -1)
+        {
+            put_nl();
+            return 1;
+        }
+        i += print_return;
+    }
+    put_nl();
+    return 0;
+}
