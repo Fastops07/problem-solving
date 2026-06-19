d07ada3336fad0d20d9028f8c01ad413213d111d
Fastops07 <matthias.voiturier@hotmail.com>
2026-06-19 17:25:49 +0200

    lcm

--- exam_rank_02/level_3/lcm/lcm.c
+++ exam_rank_02/level_3/lcm/lcm.c
@@ -1,28 +1,32 @@
-
-
 int can_divid(unsigned int nb, unsigned int i)
 {
     return (nb % i == 0);
 }
 unsigned int    lcm(unsigned int a, unsigned int b)
 {
-    unsigned int i = 2;
 
     if(a == 0 || b == 0)
         return 0;
-     while(i <= a && i <= b)
-     {
-        if(can_divid(a,i) && can_divid(b,i))
-            return i;
+    unsigned int i;
+    if(a >= b)
+        i = a;
+    else
+        i = b;
+    
+    while (1)
+    {
+        if(can_divid(i,a) && can_divid(i,b))
+            return (i);
         i++;
-     }
-     return 0;
+    }
+    return 0;
 
 }
-
+/*
 #include <stdio.h> 
 int main()
 {
-    printf("%d\n",lcm(0, 2));
+    printf("%d\n",lcm(21, -10));
     
 }
+*/
