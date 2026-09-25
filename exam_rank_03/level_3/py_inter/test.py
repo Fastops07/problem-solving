6e1c55d9b49e71e0c41c7ea1681bbf58186fcfc2
Fastops07 <matthias.voiturier@hotmail.com>
2026-09-25 22:07:55 +0200

    py inter

--- exam_rank_03/level_3/py_inter/test.py
+++ exam_rank_03/level_3/py_inter/test.py
@@ -1,19 +1,15 @@
 def inter(s1: str, s2: str) -> str:
-# PAS BON UN SET C EST PAS ORDONNE
-    commun_letter : set[str] = set()
-
+    commun_letter : list[str] = []
 
     for char in s1 :
-        if char in s2 :
-            commun_letter.add(char) 
-
-    return ""
+        if char in s2 and char not in commun_letter :
+            commun_letter.append(char)
+    return "".join(commun_letter)
 
 
 if __name__ == "__main__":
-    inter("SS1", "SB2")
-    # print(inter("hello", "world"))   # lo
-    # print(inter("banana", "band"))   # ban
-    # print(inter("abcabc", "bc"))     # bc
-    # print(inter("abc", "xyz"))       # ""
-    # print(inter("", "abc"))          # ""
+    print(inter("hello", "world"))   # lo
+    print(inter("Nbanana", "bannnnNnd"))   # ban
+    print(inter("abcabc", "bc"))     # bc
+    print(inter("abc", "xyz"))       # ""
+    print(inter("", "abc"))          # ""
